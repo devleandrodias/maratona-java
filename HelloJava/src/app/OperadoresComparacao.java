baf9bf4627292e46e7521a4d137b76abeacb2e66
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-12-14 16:56:08 -0300

    finalização de operadores de comparação

---
+++ HelloJava/src/app/OperadoresComparacao.java
@@ -0,0 +1,25 @@
+package app;
+
+/**
+ * OperadoresComparacao
+ */
+public class OperadoresComparacao {
+
+  public static void main(String[] args) {
+    boolean dezMaiorQueVinte = 10 > 20;
+    boolean dezMenorQueVinte = 10 < 20;
+    boolean dezIgualQueVinte = 10 == 20;
+
+    System.out.println("Dez é maior que 20?: " + dezMaiorQueVinte);
+    System.out.println("Dez é menor que 20?: " + dezMenorQueVinte);
+    System.out.println("Dez é igual que 20?: " + dezIgualQueVinte);
+  }
+}
+
+// Operações de comparação, todos retornam true, ou false
+// < - menor que;
+// > - maior que;
+// <= - menor igual a;
+// >= - menor igual a;
+// == - igual a;
+// != - diferente de;
