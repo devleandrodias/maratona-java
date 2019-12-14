8ceb357f057e46977c8414cd240cd8ce7cdd61c7
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-12-14 17:06:48 -0300

    Operadores lógicos

---
+++ HelloJava/src/app/OperadoresLogicos.java
@@ -0,0 +1,24 @@
+package app;
+
+/**
+ * OperadoresLogicos
+ */
+public class OperadoresLogicos {
+
+  public static void main(String[] args) {
+    boolean resposta = 10 > 5 && 10 < 20;
+    int anos = 19;
+    float salario = 2024.25f;
+    boolean respostaPodeEntrar = anos > 20 || salario > 2000f;
+
+    System.out.println("10 é maior que 5 e menor que 20?: " + resposta);
+    System.out.println("Só pode entrar pessoas maiores que 20 anos ou sálário maior R$2000,00. Você pode entrar?: "
+        + respostaPodeEntrar);
+  }
+}
+
+// Opeadores lógios, (Tabela Verdade)
+// && - E;
+// || - OU;
+// & - E a bit;
+// || - OU a bit;
