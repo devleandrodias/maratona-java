98695745075d854e029999db6ba32d1bd8668b1d
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-12-18 20:28:20 -0300

    sobrescrita de métodos e operador super

--- MaratonaJava/src/javacore/heranca/classe/Funcionario.java
+++ MaratonaJava/src/javacore/heranca/classe/Funcionario.java
@@ -1,11 +1,22 @@
 package javacore.heranca.classe;
 
 /**
- * Funcionario
+ * Não existe múltipla herança em Java. O método imprime de Funcionário
+ * sobrescreveu o método Imprime de Pessoa desde que seja igual. se não ele vira
+ * sobrecargas.
+ * 
+ * super ele chama os métodos da classe pai.
+ * 
+ * Sobrescrita de métodos
  */
 public class Funcionario extends Pessoa {
 
   private double salario;
+
+  public void imprime() {
+    super.imprime();
+    System.out.println("Salário: " + this.salario);
+  }
 
   public double getSalario() {
     return salario;
