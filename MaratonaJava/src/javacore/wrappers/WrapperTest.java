a458264ff364fb02ee0a63b9d6745e1b179908c0
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-12-21 13:37:06 -0300

    início de wrappers

---
+++ MaratonaJava/src/javacore/wrappers/WrapperTest.java
@@ -0,0 +1,50 @@
+package javacore.wrappers;
+
+/**
+ * 
+ */
+public class WrapperTest {
+
+  public static void main(String[] args) {
+    /**
+     * 
+     * Lembrar de todos os tipos primitivos.
+     * 
+     * byte bytePrimitivo = 1;
+     * 
+     * short shortPrimitivo = 1;
+     * 
+     * int intPrimitivo = 1;
+     * 
+     * long longPrimitivo = 1L;
+     * 
+     * float floatPrimitivo = 1f;
+     * 
+     * double doublePrimitivo = 12.12;
+     * 
+     * char charPrimitivo = 'A';
+     * 
+     * boolean booleanPrimitivo = true;
+     */
+
+    /**
+     * Classes Wrapper são classes que encpsulam os tipos primitivos e dar mais
+     * funcionarionadades
+     */
+
+    /**
+     * Não são mais tipos primitivos. Agora estamos trabalhando com Objetos, e tanto
+     * herança, polimorfismo, conceitos de Orientação a objetos
+     */
+
+    Byte byteWrapper = 127; // Até 127
+    Short shortWrapper = 1;
+    Integer integerWrapper = 1;
+    Long longWrapper = 1L;
+    Float floatWrapper = 1f;
+    Double doubleWrapper = 1.0;
+    Character characterWrapper = 'A';
+    Boolean booleanWrapper = true;
+
+  }
+}
