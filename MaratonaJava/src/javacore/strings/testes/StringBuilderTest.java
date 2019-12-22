fbe7ed929812b87f31037ec81d26d88bb27f9e46
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-12-22 11:49:42 -0300

    mais métodos sobre StringBuider

---
+++ MaratonaJava/src/javacore/strings/testes/StringBuilderTest.java
@@ -0,0 +1,24 @@
+package javacore.strings.testes;
+
+/**
+ * StringBuilderTest
+ */
+public class StringBuilderTest {
+
+  public static void main(String[] args) {
+    String s = "Uma string";
+    StringBuilder sb = new StringBuilder(16);
+    StringBuilder sbnum = new StringBuilder("123456789");
+
+    // Ele não criamais um objeto na memória, reutiliza mesmo objeto
+    sb.append("Uma frase comum");
+    sb.append(", novamente");
+
+    s = sb.toString();
+
+    System.out.println(s);
+    System.out.println(sb.reverse());
+    System.out.println(sbnum.insert(2, "Afgadbgdfbgdfkbg]fda")); // Começa do 1
+    System.out.println(sbnum.delete(0, 5));
+  }
+}
