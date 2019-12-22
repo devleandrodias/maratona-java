3b137c58e25a7879171105705c23a2163121f928
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-12-22 11:23:49 -0300

    string builder e string buffer

---
+++ MaratonaJava/src/javacore/strings/testes/StringPerformanceTest.java
@@ -0,0 +1,52 @@
+package javacore.strings.testes;
+
+/**
+ * StringPerformanceTest
+ */
+public class StringPerformanceTest {
+
+  public static void main(String[] args) {
+    long inicionormal = System.currentTimeMillis();
+    concatString(50000);
+    long fimnormal = System.currentTimeMillis();
+    System.out.println("Tempo gasto: " + (fimnormal - inicionormal) + "ms");
+
+    long iniciobuilder = System.currentTimeMillis();
+    concatStringBuilder(500000);
+    long fimbuilder = System.currentTimeMillis();
+    System.out.println("Tempo gasto: " + (fimbuilder - iniciobuilder) + "ms");
+
+    long iniciobuffer = System.currentTimeMillis();
+    concatStringBuffer(500000);
+    long fimbuffer = System.currentTimeMillis();
+    System.out.println("Tempo gasto: " + (fimbuffer - iniciobuffer) + "ms");
+
+  }
+
+  private static void concatString(long tam) {
+    // String string = "";
+
+    // for (int i = 0; i < tam; i++) {
+    // string += i;
+    // }
+
+  }
+
+  // Não permite multitred
+  private static void concatStringBuilder(int tam) {
+    StringBuilder stringBuilder = new StringBuilder(tam);
+
+    for (int i = 0; i < tam; i++) {
+      stringBuilder.append(i);
+    }
+  }
+
+  // São sinconizados protegidos de acessode multiplas treds
+  private static void concatStringBuffer(int tam) {
+    StringBuffer stringBuffer = new StringBuffer(tam);
+
+    for (int i = 0; i < tam; i++) {
+      stringBuffer.append(i);
+    }
+  }
+}
