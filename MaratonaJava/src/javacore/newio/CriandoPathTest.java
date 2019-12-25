3048d8def7dd6a5faddc395845ad673673ffe6d8
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-12-25 13:19:20 -0300

    introdução ao Path, Paths, Files do new io

---
+++ MaratonaJava/src/javacore/newio/CriandoPathTest.java
@@ -0,0 +1,19 @@
+package javacore.newio;
+
+// import java.nio.file.Path;
+// import java.nio.file.Paths;
+
+/**
+ * CriandoPathTest
+ */
+public class CriandoPathTest {
+
+  public static void main(String[] args) {
+    // Path p1 = Paths.get("E:\\maratona-java\\MaratonaJava\\core\\Arquivo.txt");
+    // Path p2 = Paths.get("E:\\maratona-java\\MaratonaJava\\core", "Arquivo.txt");
+    // Path p3 = Paths.get("E:", "maratona-java\\MaratonaJava\\core",
+    // "Arquivo.txt");
+    // Path p4 = Paths.get("E:", "maratona-java", "MaratonaJava", "core",
+    // "Arquivo.txt");
+  }
+}
