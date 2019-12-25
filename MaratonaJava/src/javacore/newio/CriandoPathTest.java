7597222755da2799fd57ee8b2637e9ccb5a950c5
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-12-25 13:55:55 -0300

    uso básico das classes Path, Paths e Files

--- MaratonaJava/src/javacore/newio/CriandoPathTest.java
+++ MaratonaJava/src/javacore/newio/CriandoPathTest.java
@@ -1,7 +1,9 @@
 package javacore.newio;
 
-// import java.nio.file.Path;
-// import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * CriandoPathTest
@@ -9,11 +11,37 @@ package javacore.newio;
 public class CriandoPathTest {
 
   public static void main(String[] args) {
-    // Path p1 = Paths.get("E:\\maratona-java\\MaratonaJava\\core\\Arquivo.txt");
-    // Path p2 = Paths.get("E:\\maratona-java\\MaratonaJava\\core", "Arquivo.txt");
-    // Path p3 = Paths.get("E:", "maratona-java\\MaratonaJava\\core",
-    // "Arquivo.txt");
-    // Path p4 = Paths.get("E:", "maratona-java", "MaratonaJava", "core",
-    // "Arquivo.txt");
+    Path p1 = Paths.get("E:\\maratona-java\\MaratonaJava\\core\\Arquivo.txt");
+    Path p2 = Paths.get("E:\\maratona-java\\MaratonaJava\\core", "Arquivo.txt");
+    Path p3 = Paths.get("E:", "maratona-java\\MaratonaJava\\core", "Arquivo.txt");
+    Path p4 = Paths.get("E:", "maratona-java", "MaratonaJava", "core", "Arquivo.txt");
+
+    System.out.println(p1.toAbsolutePath());
+    System.out.println(p2.toAbsolutePath());
+    System.out.println(p3.toAbsolutePath());
+    System.out.println(p4.toAbsolutePath());
+
+    // Path path1 = Paths.get("container");
+    // Path path2 = Paths.get("container\\subpasta\\subpasta");
+    Path path3 = Paths.get("container\\subpasta\\subpasta\\subpasta\\files.txt");
+    // Path arquivo = Paths.get("container\\subpasta\\file.txt");
+
+    // getParent isso pega apenas o diretório excluindo ultima parte
+    try {
+      if (Files.notExists(path3.getParent()))
+        Files.createDirectories(path3.getParent()); // Mais de um diretório
+      // Files.createFile(arquivo);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    Path source = Paths.get("E:\\maratona-java\\MaratonaJava\\core\\Arquivo.txt");
+    Path target = Paths.get("container\\subpasta\\arquivoCopiado.txt");
+
+    try {
+      Files.copy(source, target);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
