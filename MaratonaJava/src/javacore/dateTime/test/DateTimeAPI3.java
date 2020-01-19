75368aa4c0b57199a1f77dadc88ba0e9832c86d9
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-01-19 12:39:57 -0300

    mais recursos da nova API do LocalDate

---
+++ MaratonaJava/src/javacore/dateTime/test/DateTimeAPI3.java
@@ -0,0 +1,36 @@
+package javacore.dateTime.test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * DateTimeAPI3
+ */
+public class DateTimeAPI3 {
+
+  public static void main(String[] args) {
+    LocalDate localDate = LocalDate.of(2017, Month.FEBRUARY, 25);
+    System.out.println(localDate);
+
+    LocalDate localDate2 = localDate.withYear(2020); // Thread Save, se você altera o valor e cria outra
+    System.out.println(localDate2);
+
+    LocalDate localDate3 = localDate.withMonth(Month.AUGUST.getValue());
+    System.out.println(localDate3);
+
+    LocalDate dataAssinatura = LocalDate.now();
+    LocalDate dataExpiracao = dataAssinatura.withYear(dataAssinatura.getYear() + 1);
+
+    System.out.println("Data Assinatura: " + dataAssinatura);
+    System.out.println("Data Expiração: " + dataExpiracao);
+    System.out.println("Data Expiração Melhorada: " + dataAssinatura.plusYears(1));
+
+    LocalDate localDate4 = localDate2.with(ChronoField.DAY_OF_MONTH, 15);
+    System.out.println(localDate4);
+
+    System.out.println(localDate2.plusYears(1));
+    System.out.println(localDate2.plus(2, ChronoUnit.MONTHS));
+  }
+}
