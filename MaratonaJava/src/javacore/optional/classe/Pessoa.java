838da21e1df51b927d30d19a718f1aa567061414
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-01-22 20:46:20 -0300

    finalização do assunto de Optionals

--- MaratonaJava/src/javacore/optional/classe/Pessoa.java
+++ MaratonaJava/src/javacore/optional/classe/Pessoa.java
@@ -10,13 +10,15 @@ public class Pessoa {
 
   private Carro carro;
   private String nome;
+  private int idade;
 
   public Pessoa() {
   }
 
-  public Pessoa(Carro carro, String nome) {
+  public Pessoa(Carro carro, String nome, int idade) {
     this.carro = carro;
     this.nome = nome;
+    this.idade = idade;
   }
 
   public Optional<Carro> getCarro() {
@@ -25,6 +27,14 @@ public class Pessoa {
 
   public void setCarro(Carro carro) {
     this.carro = carro;
+  }
+
+  public int getIdade() {
+    return this.idade;
+  }
+
+  public void setIdade(int idade) {
+    this.idade = idade;
   }
 
   public String getNome() {
--- MaratonaJava/src/javacore/optional/test/OptionalTest2.java
+++ MaratonaJava/src/javacore/optional/test/OptionalTest2.java
@@ -14,9 +14,46 @@ public class OptionalTest2 {
   public static void main(String[] args) {
     Seguradora seguradora = new Seguradora("Santander Seguros Automotivos");
     Carro carro = new Carro(seguradora, "Kia Cerato");
-    Pessoa pessoa = new Pessoa(carro, "Leandro Dias");
-    pessoa = null;
+    Pessoa pessoa = new Pessoa(carro, "Leandro Dias", 19);
+
+    checkNomeSeguradora(seguradora);
+    checkNomeSeguradoraOptional(seguradora);
+
     getNomeSeguradora(Optional.ofNullable(pessoa));
+    System.out.println(getNomeSeguradoraComIdade(pessoa, 18));
+
+    System.out.println(stringToInt("A"));
+  }
+
+  /**
+   * 
+   * @param numero em formato string que será convertido para inteiro
+   * @return caso não seja possível converter string em um inteiro será retornado
+   *         um Optional.empty()
+   */
+  private static Optional<Integer> stringToInt(String numero) {
+    try {
+      return Optional.ofNullable(Integer.parseInt(numero));
+    } catch (NumberFormatException e) {
+      return Optional.empty();
+    }
+  }
+
+  private static void checkNomeSeguradora(Seguradora s) {
+    if (s != null && s.getNome().equals("Santander Seguros Automotivos")) {
+      System.out.println("É Seguradora do Santander");
+    }
+  }
+
+  private static void checkNomeSeguradoraOptional(Seguradora s) {
+    Optional.ofNullable(s).filter(x -> x.getNome().equals("Santander Seguros Automotivos"))
+        .ifPresent(y -> System.out.println("É Seguradora do Santander"));
+  }
+
+  public static String getNomeSeguradoraComIdade(Pessoa p, int idadeMinima) {
+    return Optional.ofNullable(p).filter(pessoa -> pessoa.getIdade() >= idadeMinima).flatMap(Pessoa::getCarro)
+        .flatMap(Carro::getSeguradora).map(Seguradora::getNome)
+        .orElse("Não existe seguradora, ou idade mínima não foi atingida");
   }
 
   /**
