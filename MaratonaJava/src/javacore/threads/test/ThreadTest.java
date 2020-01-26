9cc1f18e0ed8ead542d0306f71c23f5e0c569d25
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-01-26 12:16:26 -0300

    Sleep, Yield, Join

--- MaratonaJava/src/javacore/threads/test/ThreadTest.java
+++ MaratonaJava/src/javacore/threads/test/ThreadTest.java
@@ -9,30 +9,34 @@ package javacore.threads.test;
  * finalizadas
  */
 public class ThreadTest {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     System.out.println("Thread executada: " + Thread.currentThread().getName());
-    ThreadExamplo t1 = new ThreadExamplo('A');
-    ThreadExamplo t2 = new ThreadExamplo('B');
-    ThreadExamplo t3 = new ThreadExamplo('C');
-    ThreadExamplo t4 = new ThreadExamplo('D');
-    ThreadExamplo t5 = new ThreadExamplo('E');
+    // ThreadExamplo t1 = new ThreadExamplo('A');
+    // ThreadExamplo t2 = new ThreadExamplo('B');
+    // ThreadExamplo t3 = new ThreadExamplo('C');
+    // ThreadExamplo t4 = new ThreadExamplo('D');
+    // ThreadExamplo t5 = new ThreadExamplo('E');
 
     // t1.run(); Executa o método que roda na thread main
     // t1.start(); Executa uma thread
 
-    t1.start();
-    t2.start();
-    t3.start();
-    t4.start();
-    t5.start();
+    // t1.start();
+    // t2.start();
+    // t3.start();
+    // t4.start();
+    // t5.start();
 
-    Thread tn1 = new Thread(new ThreadExamploRunnable('A'));
-    Thread tn2 = new Thread(new ThreadExamploRunnable('B'));
-    Thread tn3 = new Thread(new ThreadExamploRunnable('C'));
-    Thread tn4 = new Thread(new ThreadExamploRunnable('D'));
-    Thread tn5 = new Thread(new ThreadExamploRunnable('E'));
+    Thread tn1 = new Thread(new ThreadExamploRunnable('A'), "T1");
+    Thread tn2 = new Thread(new ThreadExamploRunnable('B'), "T2");
+    Thread tn3 = new Thread(new ThreadExamploRunnable('C'), "T3");
+    Thread tn4 = new Thread(new ThreadExamploRunnable('D'), "T4");
+    Thread tn5 = new Thread(new ThreadExamploRunnable('E'), "T5");
 
+    tn1.setPriority(Thread.MAX_PRIORITY);
+
+    // é o main que está executando T1 main join T1
     tn1.start();
+    tn1.join();
     tn2.start();
     tn3.start();
     tn4.start();
@@ -47,7 +51,8 @@ class ThreadExamplo extends Thread {
     this.c = c;
   }
 
-  // Aqui onde tudo acontece
+  // Aqui onde tudo acontece, Scheduler
+  // Sleep, Yield, Join (Nada Garantido)
   @Override
   public void run() {
     System.out.println(Thread.currentThread().getName());
@@ -56,6 +61,16 @@ class ThreadExamplo extends Thread {
       if (i % 100 == 0) {
         System.out.println(c);
       }
+      /**
+       * Fazer a thread que estiver em execução voltar para estado Runnable e dar
+       * chance paa outras threads de mesma prioridade
+       */
+      Thread.yield();
+      // try {
+      // Thread.sleep(2000);
+      // } catch (InterruptedException e) {
+      // e.printStackTrace();
+      // }
     }
   }
 }
